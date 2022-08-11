20a1c45d165f06b08fec26f2014cd3d22e2fd04f
Annihilatopia <AnnihilateApocalypse@hotmail.com>
2022-08-11 15:08:43 +0800

    Added github actions to push to azure.

---
+++ scripts/ci_build_deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+function whitelist_address() {
+	while read address; do
+		yarn workspace eth hardhat:acadia2 whitelist:register --address "${address}"
+	done <"scripts/addresses.txt"
+}
+
+darkforest_local_hash=$(git rev-parse HEAD)
+darkforest_circuits_hash=$(git rev-parse HEAD:circuits)
+darkforest_client_hash=$(git rev-parse HEAD:client)
+darkforest_eth_hash=$(git rev-parse HEAD:eth)
+darkforest_packages_hash=$(git rev-parse HEAD:packages)
+
+echo "   darkforest-local hash: $darkforest_local_hash"
+echo "darkforest-circuits hash: $darkforest_circuits_hash"
+echo "  darkforest-client hash: $darkforest_client_hash"
+echo "     darkforest-eth hash: $darkforest_eth_hash"
+echo "darkforest-packages hash: $darkforest_packages_hash"
+
+#yarn deploy:acadia2:contracts
+yarn deploy:acadia2-whitelist:contracts && whitelist_address
+
+yarn workspace client build
+
