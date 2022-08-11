d965e78ddf082b21a80d0b0b37b5d2efaf29502b
Annihilatopia <AnnihilateApocalypse@hotmail.com>
2022-08-11 15:33:11 +0800

    Derp forgot to install dependencies lol.

--- scripts/ci_build_deploy.sh
+++ scripts/ci_build_deploy.sh
@@ -18,6 +18,8 @@ echo "  darkforest-client hash: $darkforest_client_hash"
 echo "     darkforest-eth hash: $darkforest_eth_hash"
 echo "darkforest-packages hash: $darkforest_packages_hash"
 
+yarn
+
 #yarn deploy:acadia2:contracts
 yarn deploy:acadia2-whitelist:contracts && whitelist_address
 
